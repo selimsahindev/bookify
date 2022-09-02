1bd0b5f86e7ab0d3e7ff8c61cdbe2dcfdd8994be
Selim Sahin <selimsahin.sns@gmail.com>
2022-09-02 19:33:42 +0300

    fixed typo

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 // If running in the production environment, load all the variables from .env file.
 if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').parse();
+    require('dotenv').config();
 }
 
 const express = require('express');
@@ -18,9 +18,7 @@ app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
 
-mongoose.connect(process.env.DATABASE_URL, {
-    useNewUrlParse: true,
-});
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
 
 const db = mongoose.connection;
 db.on('error', (err) => console.error(err));
