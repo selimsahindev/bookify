7a2b117b79c4cfeb4475ae9bcf0624cca34322ad
Selim Sahin <selimsahin.sns@gmail.com>
2022-09-02 23:22:59 +0300

    initial author routes

--- server.js
+++ server.js
@@ -5,18 +5,20 @@ if (process.env.NODE_ENV !== 'production') {
 
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-const app = express();
-
 const indexRouter = require('./routes/indexRoute');
+const authorRouter = require('./routes/authorsRoute');
+
+const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.set('layout', 'layouts/layout');
-
 app.use(expressLayouts);
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }));
 
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
 
@@ -25,5 +27,6 @@ db.on('error', (err) => console.error(err));
 db.once('open', () => console.log('Connected to Mongoose'));
 
 app.use('/', indexRouter);
+app.use('/authors', authorRouter);
 
 app.listen(process.env.PORT || 3000);
